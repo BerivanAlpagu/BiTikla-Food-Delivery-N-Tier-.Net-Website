39f1f70f52914f1967d10b2f5f09310466c3e56b
BerivanAlpagu <berivanalpagu@gmail.com>
2026-04-01 16:50:42 +0300

    best version of site

--- BiTikla.ConfigurationLayer/obj/Debug/net8.0/BiTikla.ConfigurationLayer.AssemblyInfo.cs
+++ BiTikla.ConfigurationLayer/obj/Debug/net8.0/BiTikla.ConfigurationLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiTikla.ConfigurationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44835a1bc2bde5e12c7b27673e574787271eb3e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae0843f71eef1a055b5154e29162f7d53e122ea8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiTikla.ConfigurationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BiTikla.ConfigurationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BiTikla.WebApi/obj/Debug/net8.0/BiTikla.WebApi.AssemblyInfo.cs
+++ BiTikla.WebApi/obj/Debug/net8.0/BiTikla.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiTikla.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44835a1bc2bde5e12c7b27673e574787271eb3e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae0843f71eef1a055b5154e29162f7d53e122ea8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiTikla.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BiTikla.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
